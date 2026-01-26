40e79fdd4c2322de7efc92795364c81a39d96b1c
zwz110 <z97881722@gmail.com>
2026-01-26 19:10:26 +0800

    modified:   zwz_blog/blog/__pycache__/models.cpython-312.pyc 	modified:   zwz_blog/blog/models.py

--- zwz_blog/blog/models.py
+++ zwz_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from django.utils.text import slugify
 from taggit.managers import TaggableManager
 
 # Create your models here.
